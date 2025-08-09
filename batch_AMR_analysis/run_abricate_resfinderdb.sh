b38eac22818f2a1a24ff1e2b0de61f4b4e7938f2
Hope Cletus Okon <92362017+Cletus-Hope@users.noreply.github.com>
2025-08-09 20:23:44 +0100

    Add files via upload

---
+++ batch_AMR_analysis/run_abricate_resfinderdb.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Define the output file
+output_file="abricate_all_dbs.csv"
+
+# Clear the output file if it exists
+> "$output_file"
+
+# List of databases
+databases=("resfinder")
+
+# Find all contigs.fa files in subdirectories and run abricate on each
+find . -type f -name "*contigs.fa" | while read -r file; do
+    for db in "${databases[@]}"; do
+        echo "Running ABRicate on $file with database: $db"
+        abricate --db "$db" "$file" >> "$output_file"
+    done
+done
+
+echo "ABRicate processing complete. Results saved to $output_file."
+
